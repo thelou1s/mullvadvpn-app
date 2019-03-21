000fbe056f7c56b89feaa89e793b2733dbfb0a29
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-03-21 10:53:13 +0000

    Bundle daemon in Android app

--- android/src/main/kotlin/net/mullvad/mullvadvpn/MainActivity.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/MainActivity.kt
@@ -1,18 +1,37 @@
 package net.mullvad.mullvadvpn
 
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 
 class MainActivity : FragmentActivity() {
+    private lateinit var extractDaemonJob: Job
+
     var selectedRelayItemCode: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
 
+        extractDaemonJob = GlobalScope.launch(Dispatchers.Default) {
+            MullvadDaemon().extract(this@MainActivity)
+        }
+
         if (savedInstanceState == null) {
             addInitialFragment()
         }
+    }
+
+    override fun onDestroy() {
+        if (extractDaemonJob.isActive) {
+            extractDaemonJob.cancel()
+        }
+
+        super.onDestroy()
     }
 
     private fun addInitialFragment() {
---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/MullvadDaemon.kt
@@ -0,0 +1,37 @@
+package net.mullvad.mullvadvpn
+
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+
+import android.content.Context
+
+private const val API_ROOT_CA_FILE = "api_root_ca.pem"
+private const val MULLVAD_DAEMON_EXE = "mullvad-daemon"
+
+private const val API_ROOT_CA_PATH = "/data/data/net.mullvad.mullvadvpn/api_root_ca.pem"
+private const val MULLVAD_DAEMON_PATH = "/data/data/net.mullvad.mullvadvpn/mullvad-daemon"
+
+class MullvadDaemon {
+    fun extract(context: Context) {
+        if (!File(API_ROOT_CA_PATH).exists()) {
+            extractFile(context, API_ROOT_CA_FILE, API_ROOT_CA_PATH)
+        }
+
+        if (!File(MULLVAD_DAEMON_PATH).canExecute()) {
+            extractFile(context, MULLVAD_DAEMON_EXE, MULLVAD_DAEMON_PATH)
+            Runtime.getRuntime().exec("/system/bin/chmod 750 $MULLVAD_DAEMON_PATH").waitFor()
+        }
+    }
+
+    private fun extractFile(context: Context, asset: String, destination: String) {
+        val destinationStream = FileOutputStream(destination)
+
+        context
+            .assets
+            .open(asset)
+            .copyTo(destinationStream)
+
+        destinationStream.close()
+    }
+}
