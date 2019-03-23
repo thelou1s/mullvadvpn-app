e813c89be3f0448cc5db89073fa6721dde515c95
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-03-23 15:05:23 +0000

    Create launch screen to decide on inital screen

--- android/src/main/kotlin/net/mullvad/mullvadvpn/MainActivity.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/MainActivity.kt
@@ -40,7 +40,7 @@ class MainActivity : FragmentActivity() {
 
     private fun addInitialFragment() {
         supportFragmentManager?.beginTransaction()?.apply {
-            add(R.id.main_fragment, LoginFragment())
+            add(R.id.main_fragment, LaunchFragment())
             commit()
         }
     }
