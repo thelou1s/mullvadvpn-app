9d2c0373f167d7ff871d2ae7d101e994740a81f1
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-03-26 12:14:04 +0000

    Expand restored selected relay item

--- android/src/main/kotlin/net/mullvad/mullvadvpn/relaylist/RelayList.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/relaylist/RelayList.kt
@@ -17,7 +17,7 @@ class RelayList {
         }
     }
 
-    fun findItemForLocation(location: LocationConstraint?): RelayItem? {
+    fun findItemForLocation(location: LocationConstraint?, expand: Boolean = false): RelayItem? {
         when (location) {
             null -> return null
             is LocationConstraint.Country -> {
@@ -26,11 +26,20 @@ class RelayList {
             is LocationConstraint.City -> {
                 val country = countries.find { country -> country.code == location.countryCode }
 
+                if (expand) {
+                    country?.expanded = true
+                }
+
                 return country?.cities?.find { city -> city.code == location.cityCode }
             }
             is LocationConstraint.Hostname -> {
                 val country = countries.find { country -> country.code == location.countryCode }
                 val city = country?.cities?.find { city -> city.code == location.cityCode }
+
+                if (expand) {
+                    country?.expanded = true
+                    city?.expanded = true
+                }
 
                 return city?.relays?.find { relay -> relay.name == location.hostname }
             }
